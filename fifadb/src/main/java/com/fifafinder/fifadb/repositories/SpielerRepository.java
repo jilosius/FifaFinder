66d3f5eb3ab0ef8bb9851402511b114f4163fb37
Wafi Kamoun <wafi.kamoun@uni-bayreuth.de>
2022-12-15 18:07:00 +0100

    Replace SpielerRepository.java

--- fifadb/src/main/java/com/fifafinder/fifadb/repositories/SpielerRepository.java
+++ fifadb/src/main/java/com/fifafinder/fifadb/repositories/SpielerRepository.java
@@ -3,7 +3,13 @@ package com.fifafinder.fifadb.repositories;
 import com.fifafinder.fifadb.entities.Spieler;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+import java.util.Optional;
 @Repository
 public interface SpielerRepository extends JpaRepository<Spieler, Integer> {
+    Optional<Spieler> findSpielerById(Integer id);
+
+    Optional<Spieler> findSpielerByfullname(String n);
+
+    void deleteSpielerById(Integer id);
+
 }
