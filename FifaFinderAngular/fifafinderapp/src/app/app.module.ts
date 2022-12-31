64e5f51dc51b07514e77087490d9d8331f2238cd
saifjilani91 <saifjilani91@gmail.com>
2022-12-31 13:16:00 +0100

    Angular Project added: Search algorithm almost done

---
+++ FifaFinderAngular/fifafinderapp/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { StartComponent } from './start/start.component';
+// import { MatMenuModule } from '@angular/material/menu'; 
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    StartComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
+    // MatMenuModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
