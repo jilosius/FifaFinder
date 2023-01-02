2d2c641ce9e27a0c6e9efee1f6611d829f97f757
saifjilani91 <saifjilani91@gmail.com>
2023-01-02 02:44:50 +0100

    Filter menu alignment modified and 'Clear Filter button now working'

--- FifaFinderAngular/fifafinderapp/src/app/start/start.component.ts
+++ FifaFinderAngular/fifafinderapp/src/app/start/start.component.ts
@@ -4,6 +4,7 @@ import { Observable, BehaviorSubject, map, startWith, catchError, of } from 'rxj
 import { ApiResponse } from '../interface/api-response';
 import { Page } from '../interface/page';
 import { SpielerService } from '../service/start.service';
+import { MatSortModule } from '@angular/material/sort';
 
 @Component({
   selector: 'app-start',
@@ -233,6 +234,58 @@ export class StartComponent implements OnInit {
     maxShortPassing?: number): void {
     this.goToPage(name, minAge, maxAge, minOverall, maxOverall, minPotential,maxPotential,minHeight,maxHeight,minValue,maxValue,minWage,maxWage,minHeadingAccuracy,maxHeadingAccuracy,minVolleys, maxVolleys,minDribbling,maxDribbling, minCurve, maxCurve,minFkAccuracy, maxFkAccuracy,minAcceleration,maxAcceleration,minSprintSpeed,maxSprintSpeed,minAgility, maxAgility, minReaction,maxReaction,minBalance,maxBalance,minShotPower,maxShotPower,minJumping,maxJumping,minStamina,maxStamina, minAggression,maxAggression, minLongShots, maxLongShots,minCrossing,maxCrossing,minFinishing,maxFinishing,minShortPassing, maxShortPassing,
                   direction === 'forward' ? this.currentPageSubject.value + 1 : this.currentPageSubject.value - 1);
+  }
+
+  resetValues(name?:string):void{
+    this.ageMin = 0;
+    this.ageMax = 100;
+    this.overallMin = 0;
+    this.overallMax = 100;
+    this.potentialMin= 0;
+    this.potentialMax= 100;
+    this.heightMin= 0;
+    this.heightMax= 300;
+    this.minValue= 0;
+    this.maxValue= 1000000000;
+    this.minWage= 0;
+    this.maxWage= 1000000000;
+    this.minHeadingAccuracy = 0;
+    this.maxHeadingAccuracy = 100;
+    this.minVolleys= 0;
+    this.maxVolleys= 100;
+    this.minDribbling= 0;
+    this.maxDribbling= 100;
+    this.minCurve= 0;
+    this.maxCurve= 100;
+    this.minFkAccuracy= 0;
+    this.maxFkAccuracy= 100;
+    this.minAcceleration= 0;
+    this.maxAcceleration= 100;
+    this.minSprintSpeed= 0;
+    this.maxSprintSpeed= 100;
+    this.minAgility= 0;
+    this.maxAgility= 100;
+    this.minReaction= 0;
+    this.maxReaction= 100;
+    this.minBalance= 0;
+    this.maxBalance= 100;
+    this.minShotPower= 0;
+    this.maxShotPower= 100;
+    this.minJumping= 0;
+    this.maxJumping= 100;
+    this.minStamina= 0;
+    this.maxStamina= 100;
+    this.minAggression= 0;
+    this.maxAggression= 100;
+    this.minLongShots= 0;
+    this.maxLongShots= 100;
+    this.minCrossing= 0;
+    this.maxCrossing= 100;
+    this.minFinishing= 0;
+    this.maxFinishing= 100;
+    this.minShortPassing= 0;
+    this.maxShortPassing= 100;
+    this.goToPage(name);
   }
 }
 
